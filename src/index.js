532145c32cf405b088dc13d269e0c52bf36b336b
Fabius Gasber <fabiusgasber@gmail.com>
2024-11-14 15:59:27 +0100

    feat: use carousel module for each carousel container class

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import { createCarousel } from "./carousel";
 import { toggleDropdown } from "./dropdown";
 import "./styles.css";
 
 const dropdowns = document.querySelectorAll(".dropdown-trigger");
 dropdowns.forEach((elem) => elem.addEventListener("click", toggleDropdown));
+
+const carouselContainer = document.querySelectorAll(".carousel-container");
+carouselContainer.forEach((carousel) => createCarousel(carousel));
