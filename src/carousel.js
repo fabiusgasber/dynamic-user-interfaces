ed11bacaccc44849bd3e1dac8d71b96f1cfebb7e
Fabius Gasber <fabiusgasber@gmail.com>
2024-11-15 13:12:02 +0100

    feat: change slide when clicking nav dots and fill them when active

--- src/carousel.js
+++ src/carousel.js
@@ -4,14 +4,20 @@ export const createCarousel = (carouselContainer) => {
   ];
   const previousBtn = carouselContainer.querySelector(".previous-btn");
   const nextBtn = carouselContainer.querySelector(".next-btn");
-  const navDots = carouselContainer.querySelector(".navigation-dots");
+  const navDots = carouselContainer.querySelector(".navigation-dots") || null;
 
   const startCarousel = () => {
     if (carouselSlides.length <= 0) return;
     toggleVisible(carouselSlides[0]);
-    addNavigationDots();
     nextBtn.addEventListener("click", nextSlide);
     previousBtn.addEventListener("click", previousSlide);
+    if (navDots) {
+      addNavigationDots();
+      updateDot();
+      nextBtn.addEventListener("click", updateDot);
+      previousBtn.addEventListener("click", updateDot);
+      navDots.addEventListener("click", changeSlide);
+    }
   };
 
   const toggleVisible = (slide) => {
@@ -19,6 +25,14 @@ export const createCarousel = (carouselContainer) => {
       slideElem !== slide
         ? slideElem.classList.remove("active")
         : slide.classList.add("active");
+    });
+  };
+
+  const toggleDotVisible = (activeDot, dots) => {
+    dots.forEach((dot) => {
+      dot !== activeDot
+        ? dot.classList.remove("active")
+        : dot.classList.add("active");
     });
   };
 
@@ -48,7 +62,26 @@ export const createCarousel = (carouselContainer) => {
       navigationDot.setAttribute("id", index);
       navDots.append(navigationDot);
     });
-  }
+  };
+
+  const updateDot = () => {
+    const dots = [
+      ...(carouselContainer.querySelectorAll(".navigation-dot") || []),
+    ];
+    const activeDot = dots[currentSlideIndex()];
+    toggleDotVisible(activeDot, dots);
+  };
+
+  const changeSlide = (e) => {
+    if (e.target.classList.contains("navigation-dot")) {
+      const index = e.target.getAttribute("id");
+      const slide = carouselSlides[index];
+      if (slide) {
+        toggleVisible(carouselSlides[index]);
+        updateDot();
+      }
+    }
+  };
 
   startCarousel();
 };
