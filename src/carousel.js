411a7050ae6bba97b8857bee910459d036bd6b68
Fabius Gasber <fabiusgasber@gmail.com>
2024-11-15 17:52:14 +0100

    refactor: carousels consists now of three modules domLoader, navdots and carousels

--- src/carousel.js
+++ src/carousel.js
@@ -1,75 +1,44 @@
 export const createCarousel = (carouselContainer) => {
-  const carouselSlides = [...(carouselContainer.querySelectorAll(".carousel-slide"))];
+  const carouselSlides = [
+    ...carouselContainer.querySelectorAll(".carousel-slide"),
+  ];
   const previousBtn = carouselContainer.querySelector(".previous-btn");
   const nextBtn = carouselContainer.querySelector(".next-btn");
-  const navDots = carouselContainer.querySelector(".navigation-dots");
 
   const navigationDot = createNavigationDots(carouselContainer);
 
   const init = () => {
-    toggleVisible(carouselSlides[0], carouselSlides);
-    setInterval(nextSlide, 5000);
+    domLoader.toggleVisible(0, carouselSlides);
     attachEventListeners();
     navigationDot.populateDots(carouselSlides);
-    updateDot();
   };
 
   const attachEventListeners = () => {
     nextBtn.addEventListener("click", nextSlide);
     previousBtn.addEventListener("click", previousSlide);
-    if (navDots) {
-      nextBtn.addEventListener("click", updateDot);
-      previousBtn.addEventListener("click", updateDot);
-      navDots.addEventListener("click", changeSlide);
-    }
-  }
-
-  const toggleVisible = (activeElem, elemArr) => {
-    if(elemArr.length <= 0) return;
-    elemArr.forEach((elem) => {
-      elem !== activeElem
-        ? elem.classList.remove("active")
-        : elem.classList.add("active");
-    });
   };
 
-  const currentSlide = () =>
-    carouselSlides.find((slide) => slide.classList.contains("active"));
-
-  const currentSlideIndex = () => carouselSlides.indexOf(currentSlide());
-
   const nextSlide = () => {
-    const currentIndex = currentSlideIndex();
-    carouselSlides[currentIndex + 1]
-      ? toggleVisible(carouselSlides[currentIndex + 1], carouselSlides)
-      : toggleVisible(carouselSlides[0], carouselSlides);
-    updateDot();
+    const currentIndex = domLoader.getCurrentIndex(carouselSlides);
+    if (carouselSlides[currentIndex + 1]){
+      domLoader.toggleVisible(currentIndex + 1, carouselSlides);
+      domLoader.toggleVisible(currentIndex + 1, navigationDot.getDotsArr());
+    }
+    else {
+      domLoader.toggleVisible(0, carouselSlides);
+      domLoader.toggleVisible(0, navigationDot.getDotsArr());
+    }
   };
 
   const previousSlide = () => {
-    const currentIndex = currentSlideIndex();
-    carouselSlides[currentIndex - 1]
-      ? toggleVisible(carouselSlides[currentIndex - 1], carouselSlides)
-      : toggleVisible(
-          carouselSlides[carouselSlides.length - 1],
-          carouselSlides,
-        );
-    updateDot();
-  };
-
-  const updateDot = () => {
-    const activeDot = navigationDot.getDot(currentSlideIndex());
-    toggleVisible(activeDot, navigationDot.getDotsArr());
-  };
-
-  const changeSlide = (e) => {
-    if (e.target.classList.contains("navigation-dot")) {
-      const index = e.target.getAttribute("id");
-      const slide = carouselSlides[index];
-      if (slide) {
-        toggleVisible(carouselSlides[index], carouselSlides);
-        updateDot();
-      }
+    const currentIndex = domLoader.getCurrentIndex(carouselSlides);
+    if(carouselSlides[currentIndex - 1]){
+      domLoader.toggleVisible(currentIndex - 1, carouselSlides)
+      domLoader.toggleVisible(currentIndex - 1, navigationDot.getDotsArr());
+    }
+    else {
+      domLoader.toggleVisible(carouselSlides.length - 1, carouselSlides);
+      domLoader.toggleVisible(carouselSlides.length - 1, navigationDot.getDotsArr());
     }
   };
 
@@ -77,11 +46,10 @@ export const createCarousel = (carouselContainer) => {
 };
 
 const createNavigationDots = (carouselContainer) => {
-
   const navDots = carouselContainer.querySelector(".navigation-dots");
-  const dots =  [];
-    
-  const getDot = (index) => dots[index] ? dots[index] : null;
+  const dots = [];
+
+  const getDot = (index) => (dots[index] ? dots[index] : null);
   const getDotsArr = () => dots;
 
   const populateDots = (carouselSlides) => {
@@ -93,8 +61,22 @@ const createNavigationDots = (carouselContainer) => {
       navDots.append(navigationDot);
       dots.push(navigationDot);
     });
+    domLoader.toggleVisible(0, dots);
   };
 
   return { getDot, getDotsArr, populateDots };
-  
-}
+};
+
+const domLoader = (() => {
+  const toggleVisible = (activeElemIndex, elemArr) => {
+    elemArr.forEach((elem) => elem.classList.remove("active"));
+    elemArr[activeElemIndex].classList.add("active");
+  };
+
+  const getCurrent = (arr) => arr.find((elem) => elem.classList.contains("active"));
+
+  const getCurrentIndex = (arr) => arr.indexOf(getCurrent(arr));
+
+
+  return { toggleVisible, getCurrentIndex };
+})();
